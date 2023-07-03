429dd8eeca1d8731eca774f6f323fcef3ed55604
YF Chau <yfchauhk@gmail.com>
2023-07-03 09:31:18 +0100

    Commit before pulling from development.

---
+++ server/database/setup.js
@@ -0,0 +1,20 @@
+// @ts-check
+
+require("dotenv").config();
+const fs = require('fs');
+const db = require('./connect');
+
+
+/// user database goes first
+
+const sql_users = fs.readFileSync("./server/database/users.sql").toString();
+
+db.query(sql_users)
+    .then(data => console.log('User table setup completed.'))
+    .catch(error => console.log(error))
+
+const sql_events = fs.readFileSync("./server/database/events.sql").toString();
+
+db.query(sql_events)
+    .then(data => console.log('Setup completed.'))
+    .catch(error => console.log(error))

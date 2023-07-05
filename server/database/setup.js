144bdb161a04a36d32f1f60f4401a858cabc765f
YF Chau <yfchauhk@gmail.com>
2023-07-05 17:22:25 +0100

    Minor refactor to setup.js in database

--- server/database/setup.js
+++ server/database/setup.js
@@ -5,17 +5,18 @@ const db = require('./connect');
 async function setupTables() {
 
     try {
-        /// **** User table MUST go first ****
+        
         const sql_users = fs.readFileSync("./server/database/users.sql").toString();
-        /// **** User table MUST go first ****
-
         const sql_admin = fs.readFileSync("./server/database/admin.sql").toString();
         const sql_participate = fs.readFileSync("./server/database/participate.sql").toString();
         const sql_volunteer = fs.readFileSync("./server/database/volunteer.sql").toString();
         const sql_events = fs.readFileSync("./server/database/events.sql").toString();
         const sql_complaints = fs.readFileSync("./server/database/complaints.sql").toString();
 
+        /// **** User table MUST go first ****
         await db.query(sql_users)
+        /// **** User table MUST go first ****
+
         await db.query(sql_admin)
         await db.query(sql_participate)
         await db.query(sql_volunteer)

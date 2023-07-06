8ce81b15c52e7ce6b44247eacc012bee35bd9ee0
Danise Rosales <daniserosales7@gmail.com>
2023-07-06 11:29:32 +0100

    signin and signout btn now working, grayed input is not yet

--- client/assets/complaints.js
+++ client/assets/complaints.js
@@ -30,3 +30,35 @@ document.getElementById("complaint-form").addEventListener("submit", async (e) =
     }
 })
 
+
+async function checkToken() {
+    // Retrieve the user's token from local storage or cookies
+    const token = await localStorage.getItem('token'); // If using 
+    if (token && token !== "") {
+      
+        document.getElementById('signin-btn').style.display = 'none';
+        document.getElementById('signout-btn').style.display = 'block';
+      console.log("User is authenticated");
+      // For example, you can show authenticated features or redirect the user to a different page
+    } else {
+        document.getElementById('signin-btn').style.display = 'block';
+        document.getElementById('signout-btn').style.display = 'none';
+      console.log("User is not authenticated");
+      // For example, you can show a sign-in form or redirect the user to a login page
+    }
+  }
+
+
+
+  document.getElementById("signout-btn").addEventListener("click", async (e) => {
+    function signOut() {
+        // Clear the user's token from local storage or cookies
+        localStorage.removeItem('token'); // If using local storage
+        if (response.status == 201) {
+            window.location.assign("home.html");
+              
+        } else {
+            alert(data.error);
+        }
+      } signOut()
+  })
--- client/assets/events.js
+++ client/assets/events.js
@@ -6,6 +6,7 @@ document.getElementById("form").addEventListener("submit", async (e) => {
     const form = new FormData(e.target);
 
     const options = {
+        'Authorization': localStorage.getItem("token"),
         method: "POST",
         headers: {
             'Accept': 'application/json',
@@ -31,3 +32,62 @@ document.getElementById("form").addEventListener("submit", async (e) => {
     }
 })
 
+async function checkToken() {
+    // Retrieve the user's token from local storage or cookies
+    const token = await localStorage.getItem('token'); // If using 
+    if (token && token !== "") {
+      
+        document.getElementById('signin-btn').style.display = 'none';
+        document.getElementById('signout-btn').style.display = 'block';
+        
+      console.log("User is authenticated");
+      // For example, you can show authenticated features or redirect the user to a different page
+    } else {
+        document.getElementById('signin-btn').style.display = 'block';
+        document.getElementById('signout-btn').style.display = 'none';
+      console.log("User is not authenticated");
+      // For example, you can show a sign-in form or redirect the user to a login page
+    }
+  }
+  function checkAuthentication() {
+    const token = localStorage.getItem('token');
+    if (token && token !== '') {
+      // User is authenticated
+      showForm();
+    } else {
+      // User is not authenticated
+      hideForm();
+    }
+  }
+  
+  // Show the form
+  function showForm() {
+    const form = document.getElementById('form');
+    form.style.display = 'block';
+  }
+  
+  // Hide the form
+  function hideForm() {
+    const form = document.getElementById('form');
+    form.style.display = 'none';
+  }
+  
+  // Call the checkAuthentication function to check the user's authentication status when the page loads
+  window.addEventListener('load', checkAuthentication);
+
+
+  document.getElementById("signout-btn").addEventListener("click", async (e) => {
+    function signOut() {
+        // Clear the user's token from local storage or cookies
+        localStorage.removeItem('token'); // If using local storage
+        if (response.status == 201) {
+            window.location.assign("home.html");
+              
+        } else {
+            alert(data.error);
+        }
+      } signOut()
+  })
+ 
+  checkToken();
+  
